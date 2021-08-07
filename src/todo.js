f5750f2e42a12a4e82feb9099ae9f585475b689c
aayushitripathi <40714458+aayushitripathi@users.noreply.github.com>
2021-08-07 15:34:46 +0530

    Delete todo.js

--- src/todo.js
+++
@@ -1,7 +0,0 @@
-const addbutton=document.getElementById("todo");
-let show=document.getElementById("display");
-const mytodo=()=>{
-    show+=document.getElementById(myinput.value);
-    return show;
-}
-addbutton.addEventListener('click',mytodo)
